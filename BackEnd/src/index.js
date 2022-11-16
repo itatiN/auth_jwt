6a63e4c601e7ec812ab1871a4d39904f7a72e79b
Itatiano <itaticontato@gmail.com>
2022-11-16 20:35:46 -0300

    ✨ Cryptography

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,27 +1,30 @@
+// mongoDB
+const key = require('./key/key');
+
 require('dotenv').config();
 
 const express = require('express');
+const morgan = require("morgan");
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
 const app = express();
+
 app.use(express.json());
 
 
-//credentials in .env
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASS;
-
 mongoose.connect(
-    `mongodb+srv://${dbUser}:${dbPassword}@jwt-auth.96g4ffp.mongodb.net/?retryWrites=true&w=majority`,
+    key,
     {
         useNewUrlParser: true
     }
 );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 app.use(require('./routes'));
 app.listen(PORT, HOST);
--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -1,4 +1,6 @@
 const routes = require("express").Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('./models/User');
 
 routes.get('/', (req, res) => {
@@ -8,8 +10,34 @@ routes.get('/', (req, res) => {
 });
 
 routes.post('/auth/register', async(req, res) => {
-    const{name, email, password, confirmpassword} = req.body;
-    if(!name) return res.status(422).json({msg: "O nome é obrigatório!"});
+    const{name, email, password, confirmPassword} = req.body;
+    
+    // validations
+    if(!name) return res.status(422).json({msg: "Name is required!"});
+    if(!email) return res.status(422).json({msg: "Email is required!"});
+    if(!password) return res.status(422).json({msg: "Password is required!"});
+    if(password !== confirmPassword) return res.status(422).json({msg: "Password not matched!"});
+
+    const userExist = await User.findOne({email:email});
+    if(userExist) return res.status(422).json({msg: "Email is already in use!"});
+
+    //cryptography
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const user = new User({
+        name,
+        email,
+        password: passwordHash
+    });
+
+    try {
+        await user.save()
+        res.status(201).json({msg: "Sucess"});
+
+    } catch (error) {
+        res.status(500).json({msg: error});
+    }
 
 });
 
